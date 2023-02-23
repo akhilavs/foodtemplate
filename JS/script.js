f3e5d310e4d8e876186c11fa3a6a53fda88917cc
akhilavs <akhilavs786@gmail.com>
2023-02-23 17:03:11 +0530

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,30 @@
 // Author:Akhila
-// Date:
+// Date:23/02/23
+function validate(){
+    let name=document.getElementById("yname").value;    
+    let n=document.getElementById("yname").value;
+    let namereg=/^[a-z A-Z]{2,15}$/;
+    let email=document.getElementById("yemail").value;
+    let ereg=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(name=="")
+    {
+        alert("Name field is empty");
+        return false;
+    }
+    else if(!namereg.test(n)){
+        alert("Enter Your currect Name");
+        return false;
+    }
+    else if(email=="")
+    {
+        alert(" Please enter your email id");
+        return false;
+    }
+    else if(!ereg.test(email)){
+        alert("Enter Your currect email id");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
